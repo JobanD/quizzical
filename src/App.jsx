e42ec77a54e8c081732335250acde27d92fb95ed
JobanD <joban.d555@gmail.com>
2022-11-08 15:19:16 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
